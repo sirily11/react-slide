814ff3c73459454796a47ffaa73177e749b9ddb5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-19 19:38:14 +0000

    Deploy sirily11/react-slide to github.com/sirily11/react-slide.git:gh-pages

---
+++ precache-manifest.d0e77b4858cfbc885c1b88325a6cc9cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45c7064542f25246a7fb47ff592048a0",
+    "url": "/react-slide/index.html"
+  },
+  {
+    "revision": "18c5cb566ffe8f9a1420",
+    "url": "/react-slide/static/css/2.9ec75927.chunk.css"
+  },
+  {
+    "revision": "55c81e571d2c990f2aad",
+    "url": "/react-slide/static/css/main.d417f268.chunk.css"
+  },
+  {
+    "revision": "18c5cb566ffe8f9a1420",
+    "url": "/react-slide/static/js/2.657243f8.chunk.js"
+  },
+  {
+    "revision": "55c81e571d2c990f2aad",
+    "url": "/react-slide/static/js/main.779c3502.chunk.js"
+  },
+  {
+    "revision": "11e282f622611523b48d",
+    "url": "/react-slide/static/js/runtime~main.74af6ff7.js"
+  }
+]);
